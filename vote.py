b383caecda54fd0cb95df17ab1ebb9cac304b067
arya271407 <arya271407@gmail.com>
2025-10-16 23:07:18 -0400

    Correct voting eligibility check and messages
    Fix syntax error in age check and update eligibility message.

--- vote.py
+++ vote.py
@@ -1,7 +1,8 @@
 try:
   age=int(input("enter your age:"))
-  if age>=18;
+  if age>=18:
+     print("you are eligible to vote")
   else:
-    print("you are eligibleto vote")
+    print("you are not eligible to vote")
 except valueError:
   print("please enter avalid integer")
